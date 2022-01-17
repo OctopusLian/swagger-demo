f25181d414ede870466444e781e055a37b29947c
OctopusLian <zoctopus@qq.com>
2022-01-17 13:38:13 +0800

    add docs/user.go

--- docs/doc.go
+++ docs/doc.go
@@ -3,7 +3,7 @@
  * @Author: neozhang
  * @Date: 2022-01-16 16:55:11
  * @LastEditors: neozhang
- * @LastEditTime: 2022-01-16 17:05:37
+ * @LastEditTime: 2022-01-17 13:15:56
  */
 
 // Package docs awesome.
@@ -31,57 +31,3 @@
 // swagger:meta
 
 package docs
-
-import (
-	"swagger-demo/api"
-)
-
-// swagger:route POST /users user createUserRequest
-// Create a user in memory.
-// responses:
-//   200: createUserResponse
-//   default: errResponse
-
-// swagger:route GET /users/{name} user getUserRequest
-// Get a user from memory.
-// responses:
-//   200: getUserResponse
-//   default: errResponse
-
-// swagger:parameters createUserRequest
-type userParamsWrapper struct {
-	// This text will appear as description of your request body.
-	// in:body
-	Body api.User
-}
-
-// This text will appear as description of your request url path.
-// swagger:parameters getUserRequest
-type getUserParamsWrapper struct {
-	// in:path
-	Name string `json:"name"`
-}
-
-// This text will appear as description of your response body.
-// swagger:response createUserResponse
-type createUserResponseWrapper struct {
-	// in:body
-	Body api.User
-}
-
-// This text will appear as description of your response body.
-// swagger:response getUserResponse
-type getUserResponseWrapper struct {
-	// in:body
-	Body api.User
-}
-
-// This text will appear as description of your error response body.
-// swagger:response errResponse
-type errResponseWrapper struct {
-	// Error code.
-	Code int `json:"code"`
-
-	// Error message.
-	Message string `json:"message"`
-}
